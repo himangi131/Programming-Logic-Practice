45e09e2684aecebac51f4d10a9f565a76a381f99
Himangi mishra <146934965+himangi131@users.noreply.github.com>
2026-08-31 18:00:12 +0530

    Add digit counting function in Count Digits.java
    Fixes syntax errors and implements a digit counting function.

---
+++ Count Digits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class counter{
+static int countDigits(int n){
+int count=0;
+while(n>0){
+n=n/10;
+count++;
+}
+return count;
+}
+Public static void main(String args[]){
+Scanner sc=new Scanner(Systeem.in);
+int n=sc.nextInt();
+Sysoln(countDigits(n));
+}
+}
