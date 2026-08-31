47a077319b5c8f4eb98dfb535fefcfb349de7029
Himangi mishra <146934965+himangi131@users.noreply.github.com>
2026-08-31 16:41:39 +0530

    Add Factorial calculation in Factorial.java
    Implement a Java program to calculate factorial.

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java .util.Scanner;
+class Factorial{
+static int Fact(int n) {
+  int res=1;
+  for(int i=1;i<=n;i++){
+    res=res*i;
+  }
+  return res;
+}
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+    sysoln(Fact(n));
+  }
+}
