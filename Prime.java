c9493a2cbea8957c172f392382abacc485a3d177
Himangi mishra <146934965+himangi131@users.noreply.github.com>
2026-09-03 23:14:43 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Prime{
+    static boolean isPrime(int n){
+        if(n < 2){
+            return false;
+        }
+        for(int i=2;i<=n/i;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(isPrime(n));
+        scanner.close();
+    }
+}
+    
